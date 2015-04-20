6286091f2fa0303a2730c5adcf75e6f4466de607
SergeAimeLesKiwis <leo.foltzrahem@gmail.com>
2015-04-20 20:52:32 +0200

    BDD

--- m2l.sql
+++ m2l.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Dim 12 Avril 2015 à 17:59
+-- Généré le :  Lun 20 Avril 2015 à 20:50
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -31,7 +31,7 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `checkCredit`(IN `id` INT)
     DETERMINISTIC
     SQL SECURITY INVOKER
 BEGIN
-  SET @credits = (SELECT credits FROM parametres);
+	SET @credits = (SELECT credits FROM parametres);
     
     SET @utilises = (SELECT SUM(form_cout_credit) FROM formation WHERE form_id IN (SELECT form_id FROM participe WHERE user_id = id AND part_statut IN (0, 1, 2, 3)));
     
@@ -52,11 +52,12 @@ DROP TABLE IF EXISTS `formation`;
 CREATE TABLE IF NOT EXISTS `formation` (
   `form_id` int(11) NOT NULL AUTO_INCREMENT,
   `form_libelle` varchar(100) NOT NULL,
-  `form_date_debut` date DEFAULT NULL,
-  `form_date_fin` date DEFAULT NULL,
-  `form_lieu` varchar(25) DEFAULT NULL,
-  `form_prerequis` varchar(25) DEFAULT NULL,
-  `form_cout_credit` int(11) DEFAULT NULL,
+  `form_contenu` varchar(250) NOT NULL,
+  `form_date_debut` date NOT NULL,
+  `form_date_fin` date NOT NULL,
+  `form_lieu` varchar(25) NOT NULL,
+  `form_prerequis` varchar(25) NOT NULL,
+  `form_cout_credit` int(11) NOT NULL,
   `prest_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`form_id`),
   KEY `FK_formation_prest_id` (`prest_id`)
@@ -66,12 +67,12 @@ CREATE TABLE IF NOT EXISTS `formation` (
 -- Contenu de la table `formation`
 --
 
-INSERT INTO `formation` (`form_id`, `form_libelle`, `form_date_debut`, `form_date_fin`, `form_lieu`, `form_prerequis`, `form_cout_credit`, `prest_id`) VALUES
-(1, 'Ninja 101', '2015-02-02', '2015-02-04', 'Tijuana', 'Que nenni', 150, 1),
-(2, 'Formation nulle', '2015-04-15', '2015-04-16', 'Loin', 'Rien', 325, 1),
-(3, 'Formation pourrie', '2015-04-01', '2015-04-04', 'Loin', 'Rien', 124, 1),
-(4, 'Formation inutile', '2015-04-26', '2015-04-28', 'Loin', 'Rien', 1, 1),
-(5, 'Formation kikoo', '2015-04-14', '2015-04-30', 'N''importe où', 'Osef', 3782, 1);
+INSERT INTO `formation` (`form_id`, `form_libelle`, `form_contenu`, `form_date_debut`, `form_date_fin`, `form_lieu`, `form_prerequis`, `form_cout_credit`, `prest_id`) VALUES
+(1, 'Ninja 101', 'contenu ninja', '2015-02-02', '2015-02-04', 'Tijuana', 'Que nenni', 150, 1),
+(2, 'Formation nulle', 'contenu nul', '2015-04-15', '2015-04-16', 'Loin', 'Rien', 325, 1),
+(3, 'Formation pourrie', 'contenu pourri', '2015-04-14', '2015-04-15', 'Loin', 'Rien', 124, 1),
+(4, 'Formation inutile', 'contenu inutile', '2015-04-26', '2015-04-28', 'Loin', 'Rien', 1, 1),
+(5, 'Formation kikoo', 'contenu kikoo', '2015-04-14', '2015-04-30', 'N''importe où', 'Osef', 3782, 1);
 
 -- --------------------------------------------------------
 
@@ -114,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `participe` (
 INSERT INTO `participe` (`form_id`, `user_id`, `part_statut`) VALUES
 (1, 2, 'demandee'),
 (2, 2, 'annulee'),
-(3, 2, 'acceptee'),
+(3, 2, 'terminee'),
 (4, 2, 'encours'),
 (5, 2, 'terminee');
 
@@ -127,10 +128,10 @@ INSERT INTO `participe` (`form_id`, `user_id`, `part_statut`) VALUES
 DROP TABLE IF EXISTS `prestataire`;
 CREATE TABLE IF NOT EXISTS `prestataire` (
   `prest_id` int(11) NOT NULL AUTO_INCREMENT,
-  `prest_raison_sociale` varchar(50) DEFAULT NULL,
-  `prest_adresse` varchar(150) DEFAULT NULL,
-  `prest_telephone` varchar(10) DEFAULT NULL,
-  `prest_siret` varchar(32) DEFAULT NULL,
+  `prest_raison_sociale` varchar(50) NOT NULL,
+  `prest_adresse` varchar(150) NOT NULL,
+  `prest_telephone` varchar(10) NOT NULL,
+  `prest_siret` varchar(32) NOT NULL,
   PRIMARY KEY (`prest_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
@@ -150,10 +151,10 @@ INSERT INTO `prestataire` (`prest_id`, `prest_raison_sociale`, `prest_adresse`, 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_login` varchar(50) DEFAULT NULL,
-  `user_mdp` varchar(75) DEFAULT NULL,
-  `user_mail` varchar(250) DEFAULT NULL,
-  `user_actif` tinyint(1) DEFAULT NULL,
+  `user_login` varchar(50) NOT NULL,
+  `user_mdp` varchar(75) NOT NULL,
+  `user_mail` varchar(250) NOT NULL,
+  `user_actif` tinyint(1) NOT NULL,
   `user_admin` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   KEY `FK_user_user_id_1` (`user_admin`)
@@ -164,7 +165,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 --
 
 INSERT INTO `user` (`user_id`, `user_login`, `user_mdp`, `user_mail`, `user_actif`, `user_admin`) VALUES
-(1, 'serge', 'eb2b0f82d5d1235eb5d5b8524ac3956e', 'sergepopolov@yopmail.com', 1, 1),
+(1, 'serge', 'eb2b0f82d5d1235eb5d5b8524ac3956e', 'sergepopolov@yopmail.com', 1, NULL),
 (2, 'pipoulefifou', 'eb2b0f82d5d1235eb5d5b8524ac3956e', 'pipou@lefifou.com', 1, 1);
 
 --
