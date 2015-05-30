f575b16d64ab84e16add425ea224bb792613e086
SergeAimeLesKiwis <leo.foltzrahem@gmail.com>
2015-05-30 18:51:27 +0200

    Changements graphiques

--- formations.php
+++ formations.php
@@ -7,6 +7,25 @@ aside($_SESSION["id"]);
 
 $user = new User($_SESSION["id"], $_SESSION["login"], $_SESSION["mail"],$_SESSION["credits"],$_SESSION["jours"]);
 
+/* Action sur les formations */
+
+if (isset($_GET["action"]) && isset($_GET["formation"]) && $_GET["formation"] != "") {
+
+	/* Inscription */
+
+	if ($_GET["action"] == "inscription") {
+		$formation = $_GET["formation"];
+
+	}
+
+	/* Désinscription */
+
+	if ($_GET["action"] == "desinscription") {
+		$formation = $_GET["formation"];
+
+	}
+}
+
 /* Liste des formations à venir */
 
 tabFormations("Liste des formations à venir", $user->getFormationsFutures());
