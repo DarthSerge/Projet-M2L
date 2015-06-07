c60ad7adfbb70ac621124fb2b2303af54a3b7e52
DarthSerge <capitolin.terry@live.fr>
2015-06-07 22:12:35 +0200

    Update2

--- classe/User.php
+++ classe/User.php
@@ -179,6 +179,18 @@ Class User {
 
 		return $data->updateFormation($this->id,$formationId,$statut);
 	}
+
+	function AddFormation($formationId){
+		$data = new DB_User();
+
+		return $data->AddFormation($this->id,$formationId);
+	}
+
+	function upDateFormation($formationId,$statut){
+		$data = new DB_User();
+
+		return $data->updateFormation($this->id,$formationId,$statut);
+	}
 }
 
 ?>
